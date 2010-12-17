f74f3ecc30c126c6f47c378706d066b35927dc07
Byron Ruth <ruthb@email.chop.edu>
2010-12-17 11:46:07 -0500

    added function to re-position the search results on window resize. improved on the comments

--- cilantro/static/js/src/define/search.js
+++ cilantro/static/js/src/define/search.js
@@ -39,10 +39,25 @@ define('define/search',
                 noresults: $('<p style="font-style:italic">No Results Found</p>')
             };
 
-            dom.results.delegate('div', 'click', function(evt) {
-                var target = $(this);
-                target.trigger(Events.ACTIVATE_CRITERION, [target.data('id')]);
-            });
+            /*
+             * Sets the position of the search results div.
+             *
+             * Defined as a function since it is used once the results are
+             * populated, but also when the window is resized.
+             */
+            function setResultsPosition() {
+
+                var rWidth = dom.results.outerWidth(),
+                    sOffset = dom.search.offset(),
+                    sHeight = dom.search.outerHeight(),
+                    sWidth = dom.search.outerWidth();
+
+                dom.results.css({
+                    left: sOffset.left - (rWidth - sWidth) / 2.0,
+                    top: sOffset.top + sHeight + 5
+                });
+
+            };
 
             // all this trouble of copying the original CriterionCollection
             // to ensure there is no discrepancy between the search results
@@ -56,30 +71,18 @@ define('define/search',
                     store: CriterionCollection._,
                     template: template
 
-                }).ready(function() {
+                }).ready(function() { 
 
                     dom.results.html(this.dom);
-
-                    /*
-                     * executes the first time the search is used.
-                     * this is mainly to ensure the DOM is ready prior to relying
-                     * on the external components such as the criteria cache and
-                     * position of the search input. secondarily, not everyone uses
-                     * the search, so it prevents performing unnecessary operations
-                     */
-
-                    var rWidth = dom.results.outerWidth(),
-                        sOffset = dom.search.offset(),
-                        sHeight = dom.search.outerHeight(),
-                        sWidth = dom.search.outerWidth();
-
-                    dom.results.css({
-                        left: sOffset.left - (rWidth - sWidth) / 2.0,
-                        top: sOffset.top + sHeight + 5
-                    });
+                    setResultsPosition();
 
                 });
 
+                /*
+                 * Performs a GET to the server for the IDs of all objects that
+                 * were a hit. The IDs are then iterated over and only those
+                 * objects are shown.
+                 */
                 dom.search.autocomplete2({
                     success: function(value, json) {
                         dom.noresults.detach();
@@ -105,32 +108,79 @@ define('define/search',
             // directly depends on the ResultCollection itself
             dom.search.bind({
 
+                /*
+                 * When the search input loses focus, make sure the user is not
+                 * interacting with the results box before hiding it.
+                 *
+                 * TODO should this fire a custom event rather than relying on
+                 * 'mouseout'?
+                 */
                 'blur': function(evt) {
                     dom.search.focused = false;
                     if (!dom.results.entered)
-                        dom.results.trigger('mouseout');
+                        dom.results.trigger('mouseleave');
                 },
                 
+                /*
+                 * If the user performed a search previously, show the state of
+                 * the results as they were.
+                 */
                 'focus': function(evt) {
                     dom.search.focused = true;
                     var val = dom.search.val();
-                    if (val && val !== dom.search.attr('placeholder'))
+                    if (val && val !== dom.search.attr('placeholder')) {
                         dom.results.fadeIn('fast');
+                    }
                 }
 
             });
 
             dom.results.bind({
 
+                /*
+                 * When the mouse is on the search results, regardless if the
+                 * search input loses focus, the results should not hide.
+                 * Therefore this flag must be set.
+                 */
                 'mouseenter': function(evt) {
                     dom.results.entered = true;
                 },
 
+                /*
+                 * Check to ensure the search input is not in focus before the
+                 * results magically go away.
+                 */
                 'mouseleave': function(evt) {
                     dom.results.entered = false;
                     if (!dom.search.focused)
                         dom.results.fadeOut('fast');
                 }
+
+            });
+
+            /*
+             * Fire the ACTIVATE_CRITERION event when any of the results are
+             * clicked.
+             *
+             * TODO there has been discussion to whether the results box should
+             * hide once clicked, or stay open.
+             */
+            dom.results.delegate('div', 'click', function(evt) {
+
+                var target = $(this);
+                target.trigger(Events.ACTIVATE_CRITERION, [target.data('id')]);
+
+            });
+
+            /*
+             * This is definitely an edge case, but a user may be in the middle
+             * of searching and decided to resize the window so we need to reset
+             * the search results position (this is only noticable when the
+             * search input is in focus).
+             */
+            $(window).bind('resize', function(evt) {
+
+                setResultsPosition();
 
             });
 
