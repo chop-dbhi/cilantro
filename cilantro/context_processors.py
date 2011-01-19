a3d65f1e609492eb727405a95fe1889cc3832f46
Byron Ruth <ruthb@email.chop.edu>
2011-01-19 16:36:14 -0500

    simplified the CILANTRO_MEDIA_URL setting by removing unecessary settings

--- cilantro/context_processors.py
+++ cilantro/context_processors.py
@@ -1,15 +1,17 @@
 from django.conf import settings
 
+MIN_DIR = 'min'
+SRC_DIR = 'src'
+
 def media(request):
     CILANTRO_MEDIA_URL = getattr(settings, 'CILANTRO_MEDIA_URL',
-        settings.MEDIA_URL)
-    CILANTRO_JS_URL = getattr(settings, 'CILANTRO_JS_URL', None)
-    
-    if CILANTRO_JS_URL is None:
-        SRC_DIR = getattr(settings, 'SRC_DIR', 'src')
-        MIN_DIR = getattr(settings, 'MIN_DIR', 'min')
-        _dir = settings.DEBUG and SRC_DIR or MIN_DIR
-        CILANTRO_JS_URL = '%sjs/%s/' % (CILANTRO_MEDIA_URL, _dir)
+        None)
+
+    if CILANTRO_MEDIA_URL is None:
+        CILANTRO_MEDIA_URL = settings.MEDIA_URL + 'client/'
+
+    CILANTRO_JS_URL = '%sjs/%s/' % (CILANTRO_MEDIA_URL,
+        settings.DEBUG and SRC_DIR or MIN_DIR)
 
     return {
         'CILANTRO_MEDIA_URL': CILANTRO_MEDIA_URL,
