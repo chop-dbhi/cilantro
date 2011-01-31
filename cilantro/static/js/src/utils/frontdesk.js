40c9b97403d19ff47d06122b5610d3bac21dfca2
millerjm1@email.chop.edu <millerjm1@email.chop.edu>
2011-01-31 08:13:39 -0500

    Working on frontdesk.

--- cilantro/static/js/src/utils/frontdesk.js
+++ cilantro/static/js/src/utils/frontdesk.js
@@ -34,8 +34,7 @@ define(function(){
     // all saved messages will be delivered, and all future messages will be delivered immediately. In this way, once an object has checked
     // in or out, the library can be used as a proxy for the object.
     // [TODO] add a greeting feature that would allow a list of messages to be delivered everytime an object checks out.
-
-    function FrontDesk(capacity){
+    function FrontDesk(capacity, jq){
         var guests = 0;
         var empty = [];
         var full = [];
@@ -44,13 +43,33 @@ define(function(){
         var rooms = {};
         var expecting = null;
         var tracking = isArray(capacity);
+        var lobby = [];
 
+        jq = jq || jQuery || $;
         // From JavaScript the Good Parts
         function isArray(val){
             if (val && typeof val === "object" && typeof val.length === "number" && !(val.propertyIsEnumerable('length'))){
                 return true;
             }
             return false;
+        }
+        // This function will issue a get on a URL, but return an object
+        // that can be reused immediately. The object will record any
+        // functions that are called on it, and when the real object is
+        // returned from the server it will replay all actions on the
+        // real object.  Once the real object is available, the fake one
+        // will just immediately proxy all calls to the real object.
+        this.fetch = function(url, constructor) {
+        // if constructor is a function, we will create an instance of
+        // it and mock it out
+        // if constructor is not present
+                         
+       
+        
+        
+        
+        
+        
         }
 
         this.onEmpty = function(cb){
@@ -60,6 +79,9 @@ define(function(){
         this.onFull = function(cb){
             full.push(cb);
         };
+        
+        // What about if only one argument is passed to checkin and out,
+        // making that be in a semaphore type thing.
 
         this.checkIn = function(room, obj){
             var item;
@@ -68,21 +90,40 @@ define(function(){
                 // We check this to make sure they haven't checked in twice
                 // This covers the situation where they have never checked in
                 // or they checked out, and came back in
-                if (status[room] !== "in"){
-                    messages[room] = [];
-                    status[room] = "in";
-                    if (obj !== undefined){
-                        rooms[room] = obj;
-                    }
-                    allCheckedIn = true;
-                    if (tracking){
-                        for (item in capacity){
-                            if (status[item] !== "in"){
-                                allCheckedIn = false;
-                                break;
-                            }
-                        }
-                    }
+                
+                // If room is not an object, assume it is an identifier
+                if (isID(room)){
+                      if (status[room] !== "in"){
+                          messages[room] = [];
+                          status[room] = "in";
+                          if (obj !== undefined){
+                              var ref = this; 
+                              jQuery(obj).once("ready",function(){
+                                  ref.checkOut(obj);
+                              });
+                              rooms[room] = obj;
+                          }
+                      } 
+                } else {
+                   // Room is an object
+                   var chair = jQuery.inArray(room, lobby);
+                   if (!~chair || (chair >=0 && !jQuery.isArray(cubby[chair]))){
+                      lobby.push(room);
+                      cubby[chair]=[];
+                      jQuery(room).once("ready", function(){
+                           ref.checkOut(room);
+                      });
+                   }
+                }
+                // TODO  might know nothing happened already here
+                allCheckedIn = true;
+                if (tracking){
+                     for (item in capacity){
+                         if (status[item] !== "in"){
+                             allCheckedIn = false;
+                             break;
+                         }
+                     }
                 }
             }else{
                 guests++;
@@ -105,17 +146,29 @@ define(function(){
             if (room === undefined){
                 guests--;
             }else{
-                 // Can't check out the same guest twice;
-                 if (status[room] === "in"){
-                     status[room] = "out";
-                     if (forwardAddress !== undefined){
-                         rooms[room] = forwardAddress;
+                if (isID(room)){
+                     // Can't check out the same guest twice;
+                     if (status[room] === "in"){
+                         status[room] = "out";
+                         if (forwardAddress !== undefined){
+                             rooms[room] = forwardAddress;
+                         }
+                         for (var i = 0, l = messages[room].length; i < l; i++){
+                             giveMessage(room,messages[room][i][0], messages[room][i][1]);
+                         }
                      }
-                     for (var i = 0, l = messages[room].length; i < l; i++){
-                         giveMessage(room,messages[room][i][0], messages[room][i][1]);
-                     }
-                 }
+                }else {
+                    var chair = jQuery.inArray(lobby);
+                    if (chair >=0 && jQuery.isArray(cubby[chair])){
+                        // Can't checkout if your not in
+                        for (var i = 0, l = cubby[chair].length; i < l; i++){
+                            giveMessage(room, cubby[chair][i][0], cubbby[chair][i][1]);
+                        } 
+                        cubby[chair]==="IN";
+                    }
+                }
             }
+            
             if (numberOfGuests() === 0){
                 $.each(empty, function(index, element){
                     element();
@@ -125,16 +178,21 @@ define(function(){
 
         this.leaveMessage = function(room, message){
             var args = Array.prototype.slice.call(arguments, 2);
-            if (status[room] === "in"){
-                messages[room].push([message, args]);
+            if isID(room){ 
+               if (status[room] === "in"){
+                   messages[room].push([message, args]);
+               }else{
+                  giveMessage(room, message, args);
+               }
             }else{
-               giveMessage(room, message, args);
+                // This is for an object
             }
         };
 
         // Determine what kind of message has been left and act accordingly
         function giveMessage(room, message, args){
-             var guest = rooms[room];
+             var guest = isID(room) ? rooms[room]:room;
+
              // Determine what kind of message has been left
              if (typeof message === "function"){
                  // Assume this a function to be called
@@ -146,10 +204,17 @@ define(function(){
              }
         }
 
+        function isId(obj){
+            return (obj === null || typeof obj.valueOf() in {"number":1, "boolean":1,"string":1});
+        }
+
         function numberOfGuests(){
              var num = 0;
              for (var key in status){
                     if (status[key] === "in") num++;
+             }
+             for (var key in cubby){
+                    if (jQuery.isArray(cubby[key])) num++;
              }
              return num+guests;
         }
