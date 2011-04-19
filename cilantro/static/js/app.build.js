08173d72493f744beaccba51799eda5eb9fca6cf
Byron Ruth <ruthb@email.chop.edu>
2011-04-19 12:27:36 -0400

    updated minified javascript

--- cilantro/static/js/app.build.js
+++ cilantro/static/js/app.build.js
@@ -1,16 +1,9 @@
 ({
-    baseUrl: '.',
-    appDir: 'src',
-    dir: 'min',
+    baseUrl: './',
+    appDir: './src',
+    dir: './min',
     optimizeCss: 'none',
-    
     modules: [{
-        name: 'lib/json2'
-    }, {
-        name: 'lib/modernizr'
-    }, {
-        name: 'lib/require-jquery'
-    }, {
         name: 'main'
     }, {
         name: 'define/main'
