51dd629412722d8935ca889b778ff89cff05b811
Byron Ruth <ruthb@email.chop.edu>
2011-04-19 11:50:19 -0400

    fixed version logic

--- cilantro/__init__.py
+++ cilantro/__init__.py
@@ -1,12 +1,14 @@
-VERSION = (2, 0, 1)
+VERSION = (2, 0, 2)
 
 def get_version():
     version = '%s.%s' % VERSION[:2]
     if version[2]:
         version = '%s.%s' % (version, VERSION[2])
-    if version[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+
+    if len(VERSION) > 3:
+        if VERSION[3:] == ('alpha', 0):
+            version = '%s pre-alpha' % version
+        else:
+            if VERSION[3] != 'final':
+                version = '%s %s %s' % (version, VERSION[3], VERSION[4])
     return version
