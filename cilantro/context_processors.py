848feebb5cdcbdd028ee016cedacd89b4c5fa829
Byron Ruth <ruthb@email.chop.edu>
2011-05-04 17:16:27 -0400

    made more legible

--- cilantro/context_processors.py
+++ cilantro/context_processors.py
@@ -1,8 +1,5 @@
 from django.conf import settings
 
-MIN_DIR = 'min'
-SRC_DIR = 'src'
-
 def media(request):
     CILANTRO_MEDIA_URL = getattr(settings, 'CILANTRO_MEDIA_URL',
         None)
@@ -10,8 +7,8 @@ def media(request):
     if CILANTRO_MEDIA_URL is None:
         CILANTRO_MEDIA_URL = settings.MEDIA_URL + 'client/'
 
-    CILANTRO_JS_URL = '%sjs/%s/' % (CILANTRO_MEDIA_URL,
-        settings.DEBUG and SRC_DIR or MIN_DIR)
+    _dir = 'src' if settings.DEBUG else 'min'
+    CILANTRO_JS_URL = '%sjs/%s/' % (CILANTRO_MEDIA_URL, _dir)
 
     return {
         'CILANTRO_MEDIA_URL': CILANTRO_MEDIA_URL,
