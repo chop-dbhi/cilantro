3f9ac1774497ff1e728c0ad60bf8e3fd1afc7345
Byron Ruth <ruthb@email.chop.edu>
2011-05-12 12:51:10 -0400

    fixed version output

--- cilantro/__init__.py
+++ cilantro/__init__.py
@@ -1,14 +1,14 @@
-VERSION = (2, 1, 2, 'beta')
+VERSION = (2, 1, 2, 'beta', 1)
 
 def get_version():
-    version = '%s.%s' % VERSION[:2]
+    version = 'v%s.%s' % VERSION[:2]
     if version[2]:
         version = '%s.%s' % (version, VERSION[2])
 
     if len(VERSION) > 3:
         if VERSION[3:] == ('alpha', 0):
-            version = '%s pre-alpha' % version
+            version = '%spre-alpha' % version
         else:
             if VERSION[3] != 'final':
-                version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+                version = '%s%s%s' % (version, VERSION[3], VERSION[4])
     return version
