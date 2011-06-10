514ac9675e929c5f6323379607a1835cfc688177
Byron Ruth <ruthb@email.chop.edu>
2011-06-10 16:51:16 -0400

    ported to Django 1.3 i.e. MEDIA_URL->STATIC_URL

--- cilantro/context_processors.py
+++ cilantro/context_processors.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 
 def media(request):
-    CILANTRO_MEDIA_URL = getattr(settings, 'CILANTRO_MEDIA_URL',
+    CILANTRO_STATIC_URL = getattr(settings, 'CILANTRO_STATIC_URL',
         None)
 
-    if CILANTRO_MEDIA_URL is None:
-        CILANTRO_MEDIA_URL = settings.MEDIA_URL + 'cilantro/'
+    if CILANTRO_STATIC_URL is None:
+        CILANTRO_STATIC_URL = settings.STATIC_URL + 'cilantro/'
 
     _dir = 'src' if settings.DEBUG else 'min'
-    CILANTRO_JS_URL = '%sjs/%s/' % (CILANTRO_MEDIA_URL, _dir)
+    CILANTRO_JS_URL = '%sjs/%s/' % (CILANTRO_STATIC_URL, _dir)
 
     return {
-        'CILANTRO_MEDIA_URL': CILANTRO_MEDIA_URL,
+        'CILANTRO_STATIC_URL': CILANTRO_STATIC_URL,
         'CILANTRO_JS_URL': CILANTRO_JS_URL
     }
