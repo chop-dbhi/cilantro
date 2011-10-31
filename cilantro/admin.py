ff87d8b5052aa050bcf24c83877acce7116248e6
Byron Ruth <b@devel.io>
2011-10-31 16:09:25 -0400

    Default to extended ModelAdmin representation

--- cilantro/admin.py
+++ cilantro/admin.py
@@ -1,8 +1,15 @@
+from django.contrib import admin
 from avocado.fields.admin import FieldAdmin
+from avocado.models import Field
 
 class ChartEnabledFieldAdmin(FieldAdmin):
     fieldsets = FieldAdmin.fieldsets[:1] + (
-        ('Charting Attributes', {
+        ('Chart Labels', {
+            'classes': ('collapse',),
             'fields': ('chart_title', 'chart_xaxis', 'chart_yaxis'),
         }),
     ) + FieldAdmin.fieldsets[1:]
+
+
+admin.site.unregister(Field)
+admin.site.register(Field, ChartEnabledFieldAdmin)
