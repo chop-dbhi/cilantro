08c9c63d25f1b045517b5566c9d3e19ca5cb712a
Byron Ruth <b@devel.io>
2011-11-16 13:13:20 -0500

    Add formutils templatetag to builtins

--- cilantro/urls.py
+++ cilantro/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, url
+from django.template.loader import add_to_builtins
+
+add_to_builtins('cilantro.templatetags.formutils')
 
 urlpatterns = patterns('cilantro.views',
     url(r'^redirect/$', 'login_redirect', name='login-redirect'),
