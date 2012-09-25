a0b0efa3676f2b4fa766ce220c330b8b1bbfe725
Byron Ruth <b@devel.io>
2012-09-25 08:18:46 -0400

    Change static context processor to leave off trailing slash This makes it a bit more clear when being used and does not conflict with being used as the baseUrl for require.js

--- cilantro/context_processors.py
+++ cilantro/context_processors.py
@@ -7,17 +7,19 @@ def media(request):
     static_url = getattr(settings, 'CILANTRO_STATIC_URL', None)
 
     if static_url is None:
-        static_url = settings.STATIC_URL + 'cilantro/'
+        static_url = settings.STATIC_URL + 'cilantro'
 
-    javascript_url = '{}scripts/javascript/{}/'.format(static_url,
+    javascript_url = '{}/scripts/javascript/{}'.format(static_url,
         'src' if settings.DEBUG else 'min')
 
-    css_url = '{}stylesheets/css/'.format(static_url)
+    css_url = '{}/stylesheets/css'.format(static_url)
+    images_url = '{}/images'.format(static_url)
 
     return {
         'CILANTRO_STATIC_URL': static_url,
         'CILANTRO_JAVASCRIPT_URL': javascript_url,
         'CILANTRO_CSS_URL': css_url,
+        'CILANTRO_IMAGES_URL': images_url,
     }
 
 
