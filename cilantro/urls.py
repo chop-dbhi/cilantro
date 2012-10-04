be6f97f25841ae9819126691af6350281ec1f7fb
Byron Ruth <b@devel.io>
2012-10-04 09:19:03 -0400

    Explicitly define and namespace Cilantro URLs

--- cilantro/urls.py
+++ cilantro/urls.py
@@ -1,6 +1,12 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url, patterns, include
 
 urlpatterns = patterns('',
-    url(r'^(?:discover/|analyze/|review/|activity/)?$', 'cilantro.views.app'),
-    url(r'^api/preferences/$', 'cilantro.views.preferences'),
+    url(r'', include(patterns('',
+        url(r'^workspace/$', 'cilantro.views.app', name='workspace'),
+        url(r'^discover/$', 'cilantro.views.app', name='discover'),
+        url(r'^analyze/$', 'cilantro.views.app', name='analyze'),
+        url(r'^review/$', 'cilantro.views.app', name='review'),
+        url(r'^activity/$', 'cilantro.views.app', name='activity'),
+        url(r'^api/preferences/$', 'cilantro.views.preferences', name='preferences'),
+    ), namespace='cilantro')),
 )
