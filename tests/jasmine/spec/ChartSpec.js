5f8e7efe1a015de1c90e7ff4b8141537c4f46480
Jeffrey Miller <jeffmax@gmail.com>
2013-02-28 12:00:23 -0500

    Test for #84 failing.

--- tests/jasmine/spec/ChartSpec.js
+++ tests/jasmine/spec/ChartSpec.js
@@ -1,20 +1,24 @@
-define(['highcharts', 'cilantro/ui/charts', 'cilantro/models', 'text!../../mock/concepts.json'], 
-    function (h, charts, models,  mocks) {
+define(['cilantro/ui/charts', 'cilantro/models', 'text!../../mock/concepts.json'], 
+    function (charts, models,  mocks) {
 
     describe("FieldDistributionChart", function(){
+       var async = new AsyncSpec(this);
        var concepts = JSON.parse(mocks);
        var model;
-       var chart = h.Chart;
+       var chart = window.Highcharts.Chart;
 
-       beforeEach(function () {
-            window.Highcharts.Chart = h.Chart = jasmine.createSpy();
+       async.beforeEach(function(done) {
+            window.Highcharts.Chart = jasmine.createSpy();
             model = new models.ConceptModel(concepts[0], { parse:true });
-       });
-       afterEach(function(){
-            window.Highcharts.Chart = h.Chart = chart;
+            done();
        });
 
-       it("should only create one Highcharts.Chart object on calls to render", function(){
+       async.afterEach(function(done){
+            window.Highcharts.Chart = chart;
+            done();
+       });
+
+       async.it("should only create one Highcharts.Chart object on calls to render", function(done){
             var testChart = new charts.FieldDistributionChart({
                  editable: false,
                  model: model.fields[0],
@@ -24,8 +28,11 @@ define(['highcharts', 'cilantro/ui/charts', 'cilantro/models', 'text!../../mock/
             });
 
            var el = testChart.render();
-           expect(h.Chart.calls.length).toEqual(1);
+
+           setTimeout(function(){
+               expect(window.Highcharts.Chart.calls.length).toEqual(1);
+               done();
+           }, 1000);
        });
     });
-
 });
