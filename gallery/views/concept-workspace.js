d1e714e9d865bafadb44a4fe2b9e8512644ee61f
Byron Ruth <b@devel.io>
2013-03-08 22:12:18 -0500

    Fix missed renaming of ConceptWorkspace

--- gallery/views/concept-workspace.js
+++ gallery/views/concept-workspace.js
@@ -2,7 +2,7 @@ define(['cilantro', 'cilantro/ui'], function(c) {
     return function(dom) {
         c.data.concepts.when(function() {
             var model = c.data.concepts.get(28)
-            var view = new c.ui.ConceptFormHistory;
+            var view = new c.ui.ConceptWorkspace;
             view.render();
             dom.html(view.el);
             view.renderItem(model);
