4814a3d044db6d3629365442d9105b7caf272f1f
Jeffrey Miller <jeffmax@gmail.com>
2013-03-15 10:35:25 -0400

    Switch to Grunt for running headless tests.

---
+++ GruntFile.js
@@ -0,0 +1,38 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    jasmine : {
+      tests: {
+      src : 'build/**/*.js',
+      options : {
+        specs : 'spec/**/*Spec.js',
+        vendor: [
+            'build/jquery.js',
+            'spec/lib/jasmine-jquery.js',
+            'spec/lib/jasmine.async.js',
+            'spec/lib/mock-ajax.js'
+        ],
+        host: 'http://127.0.0.1:8125/',
+        template:'spec/runner.tmpl', 
+        templateOptions:{
+          requireConfig: {
+             baseUrl: 'build/',
+             config: {
+                 tpl: {
+                     variable: 'data'
+                 }
+             }
+          }
+        }
+      }
+     }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+  grunt.registerTask('default', ['jasmine']);
+};
+
