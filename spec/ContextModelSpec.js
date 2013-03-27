8b88ee2b2ff162c2cba11166e5757feb8f51e436
Byron Ruth <b@devel.io>
2013-03-27 17:00:00 -0400

    Simplify API for interacting with various node types
    Each type is a subclass of ContextNodeModel with type-specific logic
    such as recursive validation and fetching for branch nodes.
    
    Field and concept ids object keys have been renamed from
    `id` and `concept_id` to `field` and `concept`. Support
    exists for this change as of Avocado 2.0.21

--- spec/ContextModelSpec.js
+++ spec/ContextModelSpec.js
@@ -1,19 +1,140 @@
-define(['cilantro', 'cilantro/ui','text!../../mock/contexts.json'], function (c, ui, mocks) {
-    describe("ContextModel", function () {
-        var contexts = JSON.parse(mocks);
+define(['cilantro', 'text!../../mock/contexts.json'], function (c, mocks) {
+
+    describe('ContextNodeModel (base class)', function() {
+        it('should validate as condition', function() {
+            var model = new c.models.ContextNodeModel({
+                field: 3,
+                operator: 'exact',
+                value: 30
+            });
+            expect(model.isValid()).toBe(true);
+        });
+
+        it('should validate as branch', function() {
+            var model = new c.models.ContextNodeModel({
+                type: 'and',
+                children: []
+            });
+
+            expect(model.isValid()).toBe(true);
+        });
+
+        it('should validate as composite', function() {
+            var model = new c.models.ContextNodeModel({
+                composite: 40
+            });
+            expect(model.isValid()).toBe(true);
+        });
+    });
+
+    describe('ConditionNodeModel', function() {
         var model;
 
-        beforeEach(function () {
-            model = new c.models.ContextModel(contexts[0], { parse:true });
+        beforeEach(function() {
+            model = new c.models.ConditionNodeModel({
+                field: 3,
+                operator: 'exact',
+                value: 30
+            });
         });
 
-        it("should contain a single node under id 30", function () {
-            expect(model.nodes[30].length).toEqual(1);
+        it('should validate', function() {
+            expect(model.isValid()).toBe(true);
+        });
+    });
+
+    describe('BranchNodeModel', function() {
+        var model, node;
+
+        beforeEach(function() {
+            model = new c.models.BranchNodeModel({
+                type: 'and',
+                children: []
+            });
+
+            node = new c.models.ConditionNodeModel({
+                field: 1,
+                concept: 1,
+                value: 30,
+                operator: 'exact'
+            });
         });
 
-        it("should have a single node labeled with concept id 28", function(){
-           expect(model.nodes[30][0].get("concept_id")).toEqual(28);
+        it('should validate', function() {
+            expect(model.isValid()).toBe(true);
         });
 
+        it('should deep validate', function() {
+            model.add(node);
+            expect(model.isValid({deep: true})).toBe(true);
+        });
+
+        it('should add', function() {
+            model.add(node);
+            expect(model.get('children').length).toBe(1);
+
+            // Adding again is a no-op
+            model.add(node);
+            expect(model.get('children').length).toBe(1);
+        });
+
+        it('should remove', function() {
+            model.add(node);
+
+            model.remove(node);
+            expect(model.get('children').length).toBe(0);
+
+            // Removing again is a no-op
+            model.remove(node);
+        });
+
+        it('should clear', function() {
+            model.add(node);
+            model.clear();
+            expect(model.get('children').length).toBe(0);
+        });
+
+        it('should not deep validate', function() {
+            model.attributes.children.push({
+                bad: 1
+            });
+            expect(model.isValid({deep: true})).toBe(false);
+        });
+
+        it('should not add itself', function() {
+            expect(function() {
+                model.add(model);
+            }).toThrow();
+        });
+
+        it('should not add bare attributes', function() {
+            expect(function() {
+                model.add(model.attributes);
+            }).toThrow();
+        });
+    });
+
+    describe('CompositeNodeModel', function() {
+        var model;
+
+        beforeEach(function() {
+            model = new c.models.CompositeNodeModel({
+                composite: 40
+            });
+        });
+
+        it('should validate', function() {
+            expect(model.isValid()).toBe(true);
+        });
+    });
+
+    describe('ContextModel', function () {
+        var contexts = JSON.parse(mocks),
+            model;
+
+        beforeEach(function() {
+            model = new c.models.ContextModel({
+            });
+        });
     });
 });
