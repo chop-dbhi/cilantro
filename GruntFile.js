9c81b714c90f6969e2f0d442bc3bdb756fc9f7b3
Byron Ruth <b@devel.io>
2013-05-13 22:52:49 -0400

    Add Grunt sass and requirejs tasks
    This is another step towards replacing the Makefile.

--- GruntFile.js
+++ GruntFile.js
@@ -1,40 +1,120 @@
 module.exports = function(grunt) {
-  'use strict';
+    'use strict';
 
-  // Project configuration.
-  grunt.initConfig({
-    jasmine : {
-      tests: {
-      src : 'build/**/*.js',
-      options : {
-        specs : 'spec/**/*Spec.js',
-        vendor: [
-            '/build/jquery.js',
-            '/spec/lib/jasmine-jquery.js',
-            '/spec/lib/jasmine.async.js',
-            '/spec/lib/mock-ajax.js'
-        ],
-        host: 'http://127.0.0.1:8125/',
-        template:'spec/runner.tmpl',
-        outfile: 'spec-runner.html',
-        keepRunner: true,
-        templateOptions:{
-          requireConfig: {
-             baseUrl: 'build/',
-             config: {
-                 tpl: {
-                     variable: 'data'
-                 }
-             }
-          }
+    grunt.initConfig({
+
+        watch: {
+            buildRunner: {
+                tasks: ['jasmine:src:build'],
+                files: ['spec/**/**/*.js']
+            },
+            sass: {
+                tasks: ['sass'],
+                files: ['scss/**/**/*.scss']
+            }
+        },
+
+        sass: {
+            dist: {
+                files: {
+                    'css/style.css': 'scss/style.scss'
+                },
+                options: {
+                    style: 'compressed',
+                    quiet: true,
+                    unixNewlines: true
+                }
+            }
+        },
+
+        requirejs: {
+            distSrc: {
+                options: {
+                    baseUrl: '.',
+                    appDir: 'build',
+                    dir: 'dist/src',
+
+                    optimize: 'none',
+                    optimizeCss: 'none',
+                    removeCombined: true,
+                    preserveLicenseComments: false,
+
+                    config: {
+                        tpl: {
+                            variable: 'data'
+                        }
+                    },
+
+                    modules: [{
+                        name: 'cilantro'
+                    }, {
+                        name: 'cilantro.ui'
+                    }]
+                }
+            },
+
+            distMin: {
+                options: {
+                    baseUrl: '.',
+                    appDir: 'build',
+                    dir: 'dist/min',
+
+                    optimize: 'uglify2',
+                    optimizeCss: 'none',
+                    removeCombined: true,
+                    preserveLicenseComments: false,
+
+                    config: {
+                        tpl: {
+                            variable: 'data'
+                        }
+                    },
+
+                    modules: [{
+                        name: 'cilantro'
+                    }, {
+                        name: 'cilantro.ui'
+                    }]
+                }
+            }
+        },
+
+        jasmine : {
+            tests: {
+                src : 'build/**/*.js',
+                options : {
+                    specs : 'spec/**/*Spec.js',
+                    vendor: [
+                        '/build/jquery.js',
+                        '/spec/lib/jasmine-jquery.js',
+                        '/spec/lib/jasmine.async.js',
+                        '/spec/lib/mock-ajax.js'
+                    ],
+                    host: 'http://127.0.0.1:8125/',
+                    template:'spec/runner.tmpl',
+                    outfile: 'spec-runner.html',
+                    keepRunner: true,
+                    templateOptions: {
+                        requireConfig: {
+                            baseUrl: 'build/',
+                            config: {
+                                tpl: {
+                                    variable: 'data'
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-      }
-     }
-    }
-  });
 
-  grunt.loadNpmTasks('grunt-contrib-jasmine');
+    });
 
-  grunt.registerTask('default', ['jasmine']);
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+    grunt.registerTask('default', ['jasmine', 'watch']);
 };
 
