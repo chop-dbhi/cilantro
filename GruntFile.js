5098f0dd289e34d673d7c3a85fd238bde70e6365
Byron Ruth <b@devel.io>
2013-05-28 09:43:17 -0400

    Fix jasmine target name for rebuilding runner

--- GruntFile.js
+++ GruntFile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
             },
 
             buildRunner: {
-                tasks: ['jasmine:src:build'],
+                tasks: ['jasmine:tests:build'],
                 files: ['spec/**/**/*.js']
             },
 
