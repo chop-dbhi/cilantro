00e131880e02f8d763cac0336d3323fefbca98b4
Byron Ruth <b@devel.io>
2013-07-08 22:13:00 -0400

    Implement separate Grunt targets and tasks for local vs. dist builds
    During development, files are symlinked, copied or compiled into the
    local/ directory. For distribution, files are copied into the dist/
    directory. JavaScript files are initially copied and CoffeeScript are
    compiled into the build/ directory. The requirejs:dist task concatenates
    and minifies a built version into dist/.

--- GruntFile.js
+++ GruntFile.js
@@ -1,145 +1,297 @@
+/*
+ * All source files are created and managed by type in `src`.
+ *
+ * There three output directories:
+ *
+ *  - `local` - Development and testing
+ *  - `build` - Intermediate directory prior to optimizations and/or
+ *      concatentation
+ *  - `dist` - Final output for release purposes. This carries over
+ *      to the master branch to be copied in the root of the repo.
+ *
+ *  Each Grunt task has a target named `local` which performs copying,
+ *  compiling or symlinking in the `local` directory for development.
+ *  Additional targets may be defined for `build` or `dist` depending on
+ *  the requirements.
+ *
+ *  Shared options for a task should be defined first, followed by each
+ *  task entry. Each task can define it's own set of options to override
+ *  the shared ones defined for the task.
+ *
+ */
+
 module.exports = function(grunt) {
     'use strict';
 
     grunt.initConfig({
 
+        srcDir: 'src',
+        specDir: 'spec',
+
+        localDir: 'local',
+        buildDir: 'build',
+        distDir: 'dist',
+
+        connect: {
+            server: {
+                options: {
+                    host: '127.0.0.1',
+                    port: 8125
+                }
+            }
+        },
+
         watch: {
             coffee: {
-                tasks: ['coffee:compile'],
+                tasks: ['coffee:local'],
                 files: [
-                    'coffee/**/*.coffee',
-                    'templates/**/*.html'
+                    '<%= srcDir %>/coffee/**/*'
                 ]
             },
 
-            buildRunner: {
-                tasks: ['jasmine:tests:build'],
-                files: ['spec/**/*.js']
+            tests: {
+                tasks: ['jasmine:local:build'],
+                files: ['<%= specDir %>/**/*']
             },
 
             sass: {
-                tasks: ['sass'],
-                files: ['scss/**/*.scss']
+                tasks: ['sass:local'],
+                files: ['<%= srcDir %>/scss/**/*']
             }
         },
 
         coffee: {
-            compile: {
+            options: {
+                bare: true,
+            },
+
+            // Output to 'local' for development
+            local: {
                 options: {
-                    bare: true,
                     sourceMap: true
                 },
                 expand: true,
-                cwd: 'coffee/',
+                cwd: '<%= srcDir %>/coffee',
+                dest: '<%= localDir %>/js',
                 src: '**/*.coffee',
-                dest: 'build',
                 rename: function(dest, src) {
                     return dest + '/' + src.replace('.coffee', '.js');
-                },
+                }
+            },
+
+            // Compiles files to the 'build' directory for distribution
+            build: {
+                expand: true,
+                cwd: '<%= srcDir %>/coffee',
+                dest: '<%= buildDir %>/js',
+                src: '**/*.coffee',
+                rename: function(dest, src) {
+                    return dest + '/' + src.replace('.coffee', '.js');
+                }
             }
         },
 
         sass: {
-            dist: {
-                files: {
-                    'css/style.css': 'scss/style.scss'
-                },
+            local: {
                 options: {
-                    //style: 'compressed',
+                    trace: true,
+                    sourcemap: true
+                },
+                files: {
+                    '<%= localDir %>/css/style.css': '<%= srcDir %>/scss/style.scss'
+                }
+            },
+
+            dist: {
+                options: {
                     quiet: true,
-                    unixNewlines: true,
-                    debugInfo: true
+                    style: 'compressed'
+                },
+                files: {
+                    '<%= distDir %>/css/style.css': '<%= srcDir %>/scss/style.scss'
                 }
             }
         },
 
         copy: {
-            main: {
+            local: {
                 files: [
-                    {expand: true, cwd:'js/', src: ['**'], dest:'build'}
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/js',
+                        src: ['**/*'],
+                        dest: '<%= localDir %>/js'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/css',
+                        src: ['**/*'],
+                        dest: '<%= localDir %>/css'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/font',
+                        src: ['**/*'],
+                        dest: '<%= localDir %>/font'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/img',
+                        src: ['**/*'],
+                        dest: '<%= localDir %>/img'
+                    }
+                ]
+            },
+
+            build: {
+                files: [
+                     {
+                        expand: true,
+                        cwd: '<%= srcDir %>/templates',
+                        src: ['**/*'],
+                        dest: '<%= buildDir %>/js/templates'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/js',
+                        src: ['**/*'],
+                        dest: '<%= buildDir %>/js'
+                    }
+                ]
+            },
+
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/css',
+                        src: ['**/*'],
+                        dest: '<%= distDir %>/css'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/font',
+                        src: ['**/*'],
+                        dest: '<%= distDir %>/font'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%= srcDir %>/img',
+                        src: ['**/*'],
+                        dest: '<%= distDir %>/img'
+                    },
+                    {
+                        expand: true,
+                        src: ['bower.json', 'package.json'],
+                        dest: '<%= distDir %>'
+                    }
                 ]
             }
         },
-        symlink: {
-            templates: {
-                relativeSrc: '../templates',
-                dest: 'build/templates',
-                options: {type:'dir'}
-            },
-            extensions: {
-                relativeSrc: '../extensions',
-                dest: 'build/ext',
-                options: {type:'dir'}
-            },
-        },
-        requirejs: {
-            distSrc: {
-                options: {
-                    baseUrl: '.',
-                    appDir: 'build',
-                    dir: 'dist/src',
 
-                    optimize: 'none',
-                    optimizeCss: 'none',
-                    removeCombined: true,
-                    preserveLicenseComments: false,
-                    config: {
-                        tpl: {
-                            variable: 'data'
-                        }
-                    },
-                    modules: [{
-                        name: 'cilantro'
-                    }, {
-                        name: 'cilantro.ui'
-                    }]
+        // Only applicable for development, e.g. local
+        symlink: {
+            options: {
+                type: 'dir'
+            },
+
+            templates: {
+                relativeSrc: '../../<%= srcDir %>/templates',
+                dest: '<%= localDir %>/js/templates'
+            },
+
+            extensions: {
+                relativeSrc: '../../<%= srcDir %>/extensions',
+                dest: '<%= localDir %>/js/extensions'
+            }
+        },
+
+        requirejs: {
+            options: {
+                baseUrl: '.',
+                inlineText: true,
+                preserveLicenseComments: false,
+                wrap: false,
+                logLevel: 1,
+                throwWhen: {
+                    optimize: true
+                },
+
+                // RequireJS plugin configs
+                config: {
+                    tpl: {
+                        variable: 'data'
+                    }
+                },
+
+                modules: [{
+                    name: 'cilantro',
+                    include: ['cilantro/ui']
+                }],
+
+                // Post analysis of built files
+                done: function(done, output) {
+                    var dups = require('rjs-build-analysis').duplicates(output);
+
+                    if (dups.length > 0) {
+                        grunt.log.subhead('Duplicates found in RequireJS build:');
+                        grunt.log.warn(dups);
+                        done(new Error('r.js built duplicate modules, please check the `excludes` option.'));
+                    }
+
+                    done();
                 }
             },
 
-            distMin: {
+            dist: {
                 options: {
-                    baseUrl: '.',
-                    appDir: 'build',
-                    dir: 'dist/min',
-
+                    appDir: 'build/js',
+                    dir: 'dist/js',
                     optimize: 'uglify2',
-                    optimizeCss: 'none',
-                    removeCombined: true,
-                    preserveLicenseComments: false,
-
-                    config: {
-                        tpl: {
-                            variable: 'data'
-                        }
-                    },
-
-                    modules: [{
-                        name: 'cilantro'
-                    }, {
-                        name: 'cilantro.ui'
-                    }]
+                    removeCombined: true
                 }
             }
         },
 
+        // Removes output directories
+        clean: {
+            local: ['local'],
+            build: ['build'],
+            dist: ['dist'],
+            postdist: [
+                '<%= distDir %>/js/templates',
+                '<%= distDir %>/js/build.txt'
+            ]
+        },
+
         jasmine : {
-            tests: {
-                src : 'build/**/*.js',
-                options : {
-                    specs : 'spec/**/*Spec.js',
-                    vendor: [
-                        '/build/jquery.js',
-                        '/spec/lib/jasmine-jquery.js',
-                        '/spec/lib/jasmine.async.js',
-                        '/spec/lib/mock-ajax.js'
-                    ],
-                    host: 'http://127.0.0.1:8125/',
-                    template:'spec/runner.tmpl',
-                    outfile: 'spec-runner.html',
-                    keepRunner: true,
+            options : {
+                specs : '<%= specDir %>/**/*',
+                host: '127.0.0.1:8125',
+                keepRunner: true,
+                template: require('grunt-template-jasmine-requirejs'),
+            },
+
+            local: {
+                options: {
                     templateOptions: {
                         requireConfig: {
-                            baseUrl: 'build/',
+                            baseUrl: '<%= localDir %>/js',
+                            config: {
+                                tpl: {
+                                    variable: 'data'
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+
+            dist: {
+                options: {
+                    templateOptions: {
+                        requireConfig: {
+                            baseUrl: '<%= distDir %>/js',
                             config: {
                                 tpl: {
                                     variable: 'data'
@@ -158,12 +310,36 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-symlink');
 
-    grunt.registerTask('default', ['jasmine', 'watch']);
-    if (grunt.file.exists('extensions/')){
-        grunt.registerTask('buildDist',['coffee','sass', 'copy', 'symlink', 'requirejs']);
-    } else {
-        grunt.registerTask('buildDist',['coffee','sass', 'copy', 'symlink:templates', 'requirejs']);
-    }
+    grunt.registerTask('local', [
+        'sass:local',
+        'coffee:local',
+        'copy:local',
+        'symlink'
+    ]);
+
+    grunt.registerTask('dist', [
+        'clean:build',
+        'coffee:build',
+        'copy:build',
+        'clean:dist',
+        'requirejs:dist',
+        'sass:dist',
+        'copy:dist',
+        'clean:postdist'
+    ]);
+
+    grunt.registerTask('work', [
+        'local',
+        'watch'
+    ]);
+
+    grunt.registerTask('test', [
+        'connect',
+        'jasmine:local'
+    ]);
+
 };
