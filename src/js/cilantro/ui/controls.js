84714ab7c61318c447fc66c50d7d5bfd2ebf58dd
Byron Ruth <b@devel.io>
2014-03-12 08:18:15 -0400

    Port controls to JavaScript
    Signed-off-by: Byron Ruth <b@devel.io>

---
+++ src/js/cilantro/ui/controls.js
@@ -0,0 +1,21 @@
+/* global define */
+
+define([
+    'underscore',
+    './controls/base',
+    './controls/range',
+    './controls/date',
+    './controls/number',
+    './controls/search',
+    './controls/infograph',
+    './controls/registry',
+    './controls/null'
+], function(_) {
+
+    // Modules to be mixed-in with exports
+    var mods = Array.prototype.slice.call(arguments, 1);
+
+    // Merge the mods into an empty object that will be exported
+    return _.extend.apply(_, [{}].concat(mods));
+
+});
