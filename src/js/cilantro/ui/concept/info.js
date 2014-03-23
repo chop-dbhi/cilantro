ec7dc09ef64e68c38752f7a531b74dd44a8c653b
Byron Ruth <b@devel.io>
2014-03-23 08:06:09 -0400

    Add ellipsis and toggle to concept description
    Fix #464
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- src/js/cilantro/ui/concept/info.js
+++ src/js/cilantro/ui/concept/info.js
@@ -15,14 +15,20 @@ define([
         ui: {
             name: '.name',
             category: '.category',
-            description: '.description'
+            description: '.description',
+            descriptionText: '.description .text',
+            descriptionToggle: '.description .toggle'
         },
 
         events: {
-            'click .category-link': 'onCategoryLinkClick'
+            'click .category-link': 'onCategoryLinkClick',
+            'click @ui.descriptionToggle': 'toggleDescription'
         },
 
         initialize: function() {
+            // For window event handler
+            _.bindAll(this, 'renderDescriptionToggle');
+
             // Assume fields have not been loaded yet. Re-render after fields
             // have been loaded.
             if (this.model.fields.length === 0) {
@@ -47,8 +53,56 @@ define([
             }
 
             return data;
-        }
+        },
 
+        onRender: function() {
+            // Initial render
+            _.defer(this.renderDescriptionToggle);
+
+            // Hide toggle on resize
+            $(window).on('resize', this.renderDescriptionToggle);
+        },
+
+        onClose: function() {
+            $(window).off('resize', this.renderDescriptionToggle);
+        },
+
+        descriptionOverflows: function() {
+            var scrollWidth = this.ui.descriptionText.prop('scrollWidth'),
+                offsetWidth = this.ui.descriptionText.prop('offsetWidth');
+
+            // Contents exceeds a "single line"
+            return scrollWidth > offsetWidth;
+        },
+
+        renderDescriptionToggle: function() {
+            // Restore state of class after test
+            var hasClass = this.ui.description.hasClass('ellipsis');
+
+            // Temporarily add class for calculation
+            this.ui.description.addClass('ellipsis');
+
+            if (this.descriptionOverflows()) {
+                this.ui.descriptionToggle.show();
+                this.ui.description.toggleClass('ellipsis', hasClass);
+            }
+            else {
+                this.ui.descriptionToggle.hide();
+                // Add it for the next resize
+                this.ui.description.addClass('ellipsis');
+            }
+        },
+
+        toggleDescription: function() {
+            // Overflow hidden is enforced, toggle normally
+            if (this.ui.description.hasClass('ellipsis')) {
+                if (!this.descriptionOverflows()) return;
+                this.ui.description.removeClass('ellipsis');
+            }
+            else {
+                this.ui.description.addClass('ellipsis');
+            }
+        }
     });
 
     return {
