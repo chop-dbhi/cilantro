49a094c9eb0f0e1d64afa1f8c660491843ae5bcf
Byron Ruth <b@devel.io>
2014-03-27 14:56:57 -0400

    Move parseTitle to models/exporter
    This is pending removal since exports *should* have a title, however
    this has been ported for the time being.
    
    Signed-off-by: Byron Ruth <b@devel.io>

--- src/js/cilantro/models/exporter.js
+++ src/js/cilantro/models/exporter.js
@@ -5,6 +5,32 @@ define([
     './base'
 ], function(_, base) {
 
+    var parseTitle = function(attrs) {
+        if (!attrs.href) return 'Untitled';
+
+        // If the title isn't there, do our best to get something meaningful
+        // from the href attribute.
+        var href = attrs.href, fields;
+
+        // Check if a trailing slash exists
+        if (href.charAt(href.length - 1) === '/') {
+            fields = href.substr(0, href.length-1).split('/');
+        }
+        else {
+            fields = href.split('/');
+        }
+
+        // Use the segment of the path as the title
+        if (fields.length > 0) {
+            return fields[fields.length - 1].toUpperCase();
+        }
+
+        // If we couldn't get the title from the href then use the whole
+        // href property to try to give some context to the title.
+        return 'Untitled ' + href;
+    };
+
+
     var ExporterModel = base.Model.extend({
         idAttribute: 'type'
     });
@@ -19,7 +45,9 @@ define([
             _.each(resp._links, function(attrs, type) {
                 // Ignore the exporter endpoint itself
                 if (type !== 'self') {
-                    models.push(_.extend({type: type}, attrs));
+                    attrs = _.extend({type: type}, attrs);
+                    if (!attrs.title) attrs.title = parseTitle(attrs);
+                    models.push(attrs);
                 }
             });
 
