9b231219355e8a3bf62d5b33208db2a455dc435c
Byron Ruth <b@devel.io>
2014-03-31 17:37:50 -0400

    2.2.23 Release
    Signed-off-by: Byron Ruth <b@devel.io>

--- src/js/cilantro/core.js
+++ src/js/cilantro/core.js
@@ -14,7 +14,7 @@ define([
 
     var cilantro = {
         // Version of cilantro
-        version: '2.2.23-beta',
+        version: '2.2.23',
 
         // Defines the minimum version and maximum version Serrano that this version
         // of Cilantro is 100% compatible with. While Cilantro will attempt to run
