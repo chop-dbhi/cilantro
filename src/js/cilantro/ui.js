9f3660478c14220e202a5d10e45de653e8268d1a
Don Naegely <naegelyd@gmail.com>
2014-04-11 11:47:14 -0400

    Include accordian in ui and update accordian element names
    Signed-off-by: Don Naegely <naegelyd@gmail.com>

--- src/js/cilantro/ui.js
+++ src/js/cilantro/ui.js
@@ -4,19 +4,20 @@
 define([
     'underscore',
     './ui/core',
+    './ui/accordian',
     './ui/base',
     './ui/button',
-    './ui/concept',
-    './ui/field',
     './ui/charts',
+    './ui/concept',
     './ui/context',
     './ui/controls',
     './ui/exporter',
-    './ui/tables',
-    './ui/query',
-    './ui/workflows',
+    './ui/field',
+    './ui/notify',
     './ui/paginator',
-    './ui/notify'
+    './ui/query',
+    './ui/tables',
+    './ui/workflows'
 ], function(_) {
 
     // Note, the ui/core module is intentially left out
