chop-dbhi/cilantro

commit 396fe1de802c8a27f8a4fe9348d8b3bf7ec62f2b

Implement vocab control for use with the harvest-vocab extensions
Signed-off-by: Byron Ruth <b@devel.io>

src/js/cilantro/ui/controls/search.js
@@ -10,7 +10,7 @@ define([
     '../paginator',
     '../values',
     '../search'
-], function($, _, Marionette, base, models, constants, paginator, values, search) {
+], function($, _, Marionette, controls, models, constants, paginator, values, search) {
 
     // Single page of values
     var SearchPageModel = models.Page.extend({
@@ -36,39 +36,19 @@ define([
         initialize: function(items, options) {
             options = options || {};
             this.field = options.field;
+            this.currentUrl = null;
             models.Paginator.prototype.initialize.call(this, items, options);
         },
 
         url: function() {
-            var url = this.field.links.values;
+            var url = this.currentUrl || this.field.links.values;
+
             if (this.urlParams) {
                 url = url + '?' + $.param(this.urlParams);
             }
             return url;
         }
     });
-
-
-    // View for querying field values
-    var ValueSearch = search.Search.extend({
-        className: 'field-search search',
-
-        initialize: function() {
-            search.Search.prototype.initialize.call(this);
-            this.paginator = this.options.paginator;
-        },
-
-        search: function(query) {
-            if (query) {
-                this.options.paginator.urlParams = {query: query};
-            } else {
-                this.paginator.urlParams = null;
-            }
-
-            this.paginator.refresh();
-        }
-    });
-
 
     // Single search result item
     var SearchItem = Marionette.ItemView.extend({
@@ -139,7 +119,7 @@ define([
     });
 
 
-    var SearchControl = base.ControlLayout.extend({
+    var SearchControl = controls.ControlLayout.extend({
         className: 'field-value-search',
 
         template: 'controls/search/layout',
@@ -158,7 +138,7 @@ define([
         },
 
         regionViews: {
-            search: ValueSearch,
+            search: search.Search,
             paginator: paginator.Paginator,
             browse: SearchPageRoll,
             values: values.ValueList
@@ -190,9 +170,11 @@ define([
         onRender: function() {
             var searchRegion = new this.regionViews.search({
                 model: this.model,
-                paginator: this.valuesPaginator,
                 placeholder: 'Search ' + this.model.get('plural_name') + '...'
             });
+
+            // Listen to search events
+            this.listenTo(searchRegion, 'search', this.handleSearch);
 
             var browseRegion = new this.regionViews.browse({
                 collection: this.valuesPaginator,
@@ -214,8 +196,13 @@ define([
             this.values.show(valuesRegion);
         },
 
+        handleSearch: function(query) {
+            this.paginator.urlParams = query ? {query: query} : null;
+            this.paginator.refresh();
+        },
+
         clearValues: function() {
-           this.values.currentView.clear();
+            this.values.currentView.clear();
         },
 
         getField: function() {
@@ -278,7 +265,6 @@ define([
 
     return {
         SearchControl: SearchControl,
-        ValueSearch: ValueSearch,
         SearchItem: SearchItem,
         SearchPage: SearchPage,
         SearchPageRoll: SearchPageRoll,

src/js/cilantro/ui/controls.js
@@ -10,7 +10,8 @@ define([
     './controls/infograph',
     './controls/registry',
     './controls/null',
-    './controls/text'
+    './controls/text',
+    './controls/vocab'
 ], function(_) {
 
     // Modules to be mixed-in with exports

src/js/cilantro/ui/controls/registry.js
@@ -10,8 +10,9 @@ define([
     './select',
     './infograph',
     './null',
-    './text'
-], function(_, loglevel, c, date, number, search, select, infograph, nullSelector, text) {
+    './text',
+    './vocab'
+], function(_, loglevel, c, date, number, search, select, infograph, nullSelector, text, vocab) {
 
     var defaultControls = {
         infograph: infograph.InfographControl,
@@ -21,7 +22,8 @@ define([
         singleSelectionList: select.SingleSelectionList,
         multiSelectionList: select.MultiSelectionList,
         nullSelector: nullSelector.NullSelector,
-        text: text.TextControl
+        text: text.TextControl,
+        vocab: vocab.VocabControl
     };
 
     var customControls = {};

src/js/cilantro/ui/controls/vocab.js
@@ -1,0 +1,513 @@
+/* global define */
+
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'marionette',
+    './base',
+    './search',
+    '../search',
+    '../values',
+    '../paginator',
+    '../../models',
+], function($, _, Backbone, Marionette, controls, searchControl, search, values,
+            paginator, models) {
+
+
+    // Collection that listens to another collection for models that match
+    // certain criteria. Models that match will be added/removed from
+    // this collection.
+    var FilteredCollection = Backbone.Collection.extend({
+        _matches: function(model) {
+            var attrs = model.attributes || model;
+            return this.matcher(attrs);
+        },
+
+        initialize: function(models, options) {
+            this.matcher = options.matcher;
+            this.values = options.collection;
+
+            this.listenTo(this.values, {
+                change: function(model, options) {
+                    if (this._matches(model)) {
+                        this.add(model, options);
+                    } else {
+                        this.remove(model, options);
+                    }
+                },
+                add: function(model, collection, options) {
+                    if (this._matches(model)) this.add(model, options);
+                },
+                remove: function(model, collection, options) {
+                    if (this._matches(model)) this.remove(model, options);
+                },
+                reset: function(models, collection, options) {
+                    var _models = null;
+                    if (!models || !models.length) {
+                        _models = [];
+                    } else {
+                        _models = _.filter(models, this._matches);
+                    }
+                    if (_models) this.reset(_models, options);
+                }
+            });
+        }
+    });
+
+
+    // Extend value model to ensure the default operator is defined
+    var BucketValue = models.Value.extend({
+        defaults: {
+            operator: 'in'
+        }
+    });
+
+
+    var BucketValues = models.Values.extend({
+        model: BucketValue
+    });
+
+
+    // Single item in a bucket
+    var BucketItem = values.ValueItem.extend({
+        template: 'controls/vocab/bucket-item',
+
+        ui: {
+            remove: '[data-target=remove]'
+        },
+
+        events: {
+            'click @ui.remove': 'removeItem'
+        },
+
+        removeItem: function() {
+            this.model.destroy();
+        }
+    });
+
+
+    // Buckets are implicitly bound to an operator via the passed collection.
+    // The bucket name must be passed as an option.
+    var Bucket = Marionette.CompositeView.extend({
+        className: 'vocab-bucket',
+
+        template: 'controls/vocab/bucket',
+
+        itemView: BucketItem,
+
+        itemViewContainer: '[data-target=items]',
+
+        options: {
+            name: 'Bucket'
+        },
+
+        ui: {
+            items: '[data-target=items]'
+        },
+
+        events: {
+            'sortreceive [data-target=items]': 'receiveItem'
+        },
+
+        collectionEvents: {
+            'add remove reset': 'renderListState'
+        },
+
+        serializeData: function() {
+            return {
+                name: this.options.name
+            };
+        },
+
+        // jQuery UI does not trigger an event on the item itself, only the
+        // lists themselves. The `ui.item` is the bucket item DOM element
+        // which can be used to find the collection item.
+        receiveItem: function(event, ui) {
+            // Find the model based on the value
+            var value = ui.item.find('[data-value]').data('value'),
+                model = this.collection.values.findWhere({value: parseInt(value, 10)});
+
+            model.set('operator', this.options.operator);
+        },
+
+        onRender: function() {
+            this.ui.items.sortable({
+                forcePlaceholderSize: true,
+                forceHelperSize: true,
+                placeholder: 'placeholder',
+                scroll: false,
+                opacity: 0.7,
+                cursor: 'move',
+                connectWith: '.vocab-bucket [data-target=items]'
+            });
+
+            this.renderListState();
+        },
+
+        renderListState: function() {
+            this.$el.toggleClass('empty', this.collection.length === 0);
+        }
+
+    });
+
+
+    // View that initializes a filtered collection and renders a bucket for each
+    // available operator.
+    var BucketList = Marionette.View.extend({
+        initialize: function() {
+            var name = this.model.get('alt_name').toLowerCase(),
+                pluralName = this.model.get('alt_plural_name').toLowerCase();
+
+            this.buckets = [{
+                name: 'At least one ' + name + ' must match',
+
+                operator: 'in',
+
+                collection: new FilteredCollection(null, {
+                    collection: this.collection,
+                    matcher: function(attrs) {
+                        return attrs.operator === 'in';
+                    }
+                })
+            }, {
+                name: 'All the ' + pluralName + ' must match',
+
+                operator: 'all',
+
+                collection: new FilteredCollection(null, {
+                    collection: this.collection,
+                    matcher: function(attrs) {
+                        return attrs.operator === 'all';
+                    }
+                })
+            }, {
+                name: 'The combination of '+ pluralName + ' cannot match',
+
+                operator: '-all',
+
+                collection: new FilteredCollection(null, {
+                    collection: this.collection,
+                    matcher: function(attrs) {
+                        return attrs.operator === '-all';
+                    }
+                })
+            }, {
+                name: 'None of the ' + pluralName + ' can match',
+
+                operator: '-in',
+
+                collection: new FilteredCollection(null, {
+                    collection: this.collection,
+                    matcher: function(attrs) {
+                        return attrs.operator === '-in';
+                    }
+                })
+            }];
+        },
+
+        render: function() {
+            for (var i = 0; i < this.buckets.length; i++) {
+                var bucket = new Bucket(this.buckets[i]);
+                bucket.render();
+                this.$el.append(bucket.el);
+            }
+            return this.el;
+        }
+    });
+
+
+    // Region displaying the current path and button for going up the stack
+    var Path = Marionette.ItemView.extend({
+        template: 'controls/vocab/path',
+
+        className: 'vocab-path'
+    });
+
+
+    // Single search result item
+    var VocabItem = Marionette.ItemView.extend({
+        className: 'value-item',
+
+        template: 'controls/vocab/item',
+
+        ui: {
+            actions: '.actions',
+            addButton: '.add-item-button',
+            removeButton: '.remove-item-button'
+        },
+
+        events: {
+            'click .add-item-button': 'addItem',
+            'click .remove-item-button': 'removeItem'
+        },
+
+        constructor: function(options) {
+            options = options || {};
+
+            if ((this.values = options.values)) {
+                this.listenTo(this.values, 'add', this.setState);
+                this.listenTo(this.values, 'remove', this.setState);
+                this.listenTo(this.values, 'reset', this.setState);
+            }
+
+            Marionette.ItemView.prototype.constructor.call(this, options);
+        },
+
+        serializeData: function() {
+            var link = this.model.get('_links').children;
+
+            return {
+                url: link ? link.href : null,
+                label: this.model.get('label')
+            };
+        },
+
+        addItem: function() {
+            // Mark as valid since it was derived from a controlled list
+            var attrs = _.extend(this.model.toJSON(), {valid: true});
+            this.values.add(attrs);
+        },
+
+        removeItem: function() {
+            this.values.remove(this.model);
+        },
+
+        setState: function() {
+            if (!!this.values.get(this.model)) {
+                this.ui.addButton.hide();
+                this.ui.removeButton.show();
+            } else {
+                this.ui.addButton.show();
+                this.ui.removeButton.hide();
+            }
+        },
+
+        onRender: function() {
+            this.setState();
+        }
+    });
+
+
+    // A single page of search results
+    var VocabPage = paginator.ListingPage.extend({
+        className: 'search-value-list',
+
+        itemView: VocabItem
+    });
+
+
+    // All search result pages, only the current page is shown, the rest are
+    // hidden.
+    var VocabPageRoll = paginator.PageRoll.extend({
+        listView: VocabPage
+    });
+
+
+    var VocabControl = controls.ControlLayout.extend({
+        className: 'vocab-control',
+
+        template: 'controls/vocab/layout',
+
+        events: {
+            'click [data-action=clear]': 'clearValues',
+            'click .browse-region a': 'triggerDescend',
+            'click .path-region button': 'triggerAscend'
+        },
+
+        options: {
+            resetPathOnSearch: false
+        },
+
+        regions: {
+            paginator: '.paginator-region',
+            search: '.search-region',
+            path: '.path-region',
+            browse: '.browse-region',
+            buckets: '.buckets-region'
+        },
+
+        regionViews: {
+            search: search.Search,
+            paginator: paginator.Paginator,
+            path: Path,
+            browse: VocabPageRoll,
+            buckets: BucketList
+        },
+
+        initialize: function() {
+            // Initialize a new collection of values that centralizes the
+            // selected values.
+            this.selectedValues = new BucketValues();
+
+            // Trigger a change event on all collection events
+            this.listenTo(this.selectedValues, 'all', this.change);
+
+            this.valuesPaginator = new searchControl.SearchPaginator(null, {
+                field: this.model
+            });
+
+            this._path = new Backbone.Collection();
+        },
+
+        triggerDescend: function(event) {
+            event.preventDefault();
+            event.stopPropagation();
+
+            var target = $(event.target);
+
+            // Push item onto the stack; triggers refresh downstream
+            this._path.push({
+                label: target.text(),
+                url: target.prop('href')
+            });
+        },
+
+        triggerAscend: function() {
+            event.preventDefault();
+            event.stopPropagation();
+
+            // Popitem off stack; triggers refresh downstream
+            this._path.pop();
+        },
+
+        refreshPaginator: function() {
+            var model = this._path.last();
+
+            this.path.show(new this.regionViews.path({
+                model: model
+            }));
+
+            this.valuesPaginator.currentUrl = model.get('url');
+            this.valuesPaginator.refresh();
+        },
+
+        onRender: function() {
+            // When a search occurs, the paginator is reset to use the
+            // URL with the parameters. When the search is cleared, the
+            // default URL is used (accessing the root).
+            var searchRegion = new this.regionViews.search({
+                model: this.model,
+                placeholder: 'Search ' + this.model.get('plural_name') + '...',
+            });
+
+            this.listenTo(searchRegion, 'search', this.handleSearch);
+
+            // Click events from valid path links in the browse region
+            // will cause the paginator to refresh
+            var browseRegion = new this.regionViews.browse({
+                collection: this.valuesPaginator,
+                values: this.selectedValues
+            });
+
+            var paginatorRegion = new this.regionViews.paginator({
+                className: 'paginator mini',
+                model: this.valuesPaginator
+            });
+
+            var bucketsRegion = new this.regionViews.buckets({
+                model: this.model,
+                collection: this.selectedValues
+            });
+
+            this.search.show(searchRegion);
+            this.browse.show(browseRegion);
+            this.paginator.show(paginatorRegion);
+            this.buckets.show(bucketsRegion);
+
+            this.listenTo(this._path, 'add remove', this.refreshPaginator);
+
+            // Add top-level (default) which will render
+            this._path.push({label: 'Top-level'});
+        },
+
+        handleSearch: function(query) {
+            if (this.options.resetPathOnSearch) {
+                this.valuesPaginator.currentUrl = null;
+            }
+
+            this.valuesPaginator.urlParams = query ? {query: query} : null;
+            this.valuesPaginator.refresh();
+        },
+
+        get: function() {
+            // Object of operator keys and array values
+            var operator, values = {};
+
+            // Build operator/values map
+            this.selectedValues.each(function(model) {
+                operator = model.get('operator');
+
+                // Initialize the array if this is the first value
+                // for this operator
+                if (!values[operator]) values[operator] = [];
+
+                values[operator].push(model.pick('label', 'value'));
+            });
+
+            var operators = _.keys(values);
+
+            // No values selected
+            if (!operators.length) return;
+
+            // Single operator, return single condition
+            if (operators.length === 1) {
+                return {
+                    field: this.model.id,
+                    operator: operators[0],
+                    value: values[operators[0]]
+                };
+            }
+
+            // Multiple operators, return branch of conditions
+            return {
+                type: 'and',
+                children: _.map(values, function(values, operator) {
+                    return {
+                        field: this.model.id,
+                        operator: operator,
+                        value: values
+                    };
+                }, this)
+            };
+        },
+
+        _mapSetValues: function(values, operator) {
+            return _.map(values, function(value) {
+                // Value is expected to be an object with label and value keys.
+                value = _.clone(value);
+                value.operator = operator;
+                return value;
+            });
+        },
+
+        // The expected structure is either null, a single condition or
+        // a branch of one or more conditions. Values are collected and
+        // tagged with the operator that is applied to the values.
+        set: function(attrs) {
+            if (!attrs) return;
+
+            var values = [];
+
+            if (attrs.children) {
+                // Each child is a condition with an array of values
+                _.each(attrs.children, function(child) {
+                    values = values.concat(this._mapSetValues(child.value,
+                                                              child.operator));
+                }, this);
+            } else {
+                values = this._mapSetValues(attrs.value, attrs.operator);
+            }
+
+            // Do not remove values in case they are new since the set
+            // has occurred.
+            this.selectedValues.set(values, {remove: false});
+        }
+
+    });
+
+    return {
+        VocabControl: VocabControl
+    };
+
+});

src/js/cilantro/ui/templates.js
@@ -66,6 +66,12 @@ define([
     'tpl!templates/controls/text/preview-list.html',
     'tpl!templates/controls/text/preview-item.html',
 
+    'tpl!templates/controls/vocab/layout.html',
+    'tpl!templates/controls/vocab/path.html',
+    'tpl!templates/controls/vocab/item.html',
+    'tpl!templates/controls/vocab/bucket.html',
+    'tpl!templates/controls/vocab/bucket-item.html',
+
     'tpl!templates/field/form-condensed.html',
     'tpl!templates/field/form.html',
     'tpl!templates/field/info.html',
@@ -79,6 +85,7 @@ define([
     'tpl!templates/query/list.html',
     'tpl!templates/query/loader.html',
 
+    'tpl!templates/values/item.html',
     'tpl!templates/values/list.html',
 
     'tpl!templates/workflows/query.html',

src/js/cilantro/ui/values.js
@@ -10,6 +10,13 @@ define([
     '../constants'
 ], function($, _, Marionette, base, c, models, constants) {
 
+    var ValueItem = Marionette.ItemView.extend({
+        className: 'value-item',
+
+        template: 'values/item'
+    });
+
+
     // Interface for representing a user-defined/selected list of values
     // in a textarea (one per line). In majority of cases, values
     // correspond to the labels, but in certain cases, the underlying
@@ -108,6 +115,7 @@ define([
     });
 
     return {
+        ValueItem: ValueItem,
         ValueList: ValueList
     };
 

src/scss/controls/_vocab.scss
@@ -1,0 +1,42 @@
+.vocab-control {
+    .search input {
+        margin-bottom: 0;
+    }
+
+    .vocab-path .path-label {
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: middle;
+        display: inline-block;
+        width: 84%;
+    }
+
+    .vocab-bucket {
+        margin-bottom: 20px;
+
+        .value-list {
+            min-height: 26px;
+
+            border: 1px solid #ccc;
+
+            .value-item:last-child {
+                margin-bottom: 0;
+            }
+
+        }
+
+        &.empty {
+            .value-list {
+                border: 1px dashed #aaa;
+            }
+        }
+
+        .placeholder {
+            min-height: 26px;
+            margin-bottom: 0;
+            background-color: lightyellow;
+        }
+
+    }
+}

src/scss/style.scss
@@ -49,6 +49,8 @@
 
 @import "views/text-preview";
 
+@import "controls/vocab";
+
 @import "workflows/workspace";
 @import "workflows/query";
 @import "workflows/results";

commit d4bcdfe91cb8284e0f22ab18fc44e342e5c06257

Fix incorrect reference to paginator
This bug was introduced in 396fe1de

Fix #573

Signed-off-by: Byron Ruth <b@devel.io>

src/js/cilantro/ui/controls/search.js
@@ -203,8 +203,8 @@ define([
         },
 
         handleSearch: function(query) {
-            this.paginator.urlParams = query ? {query: query} : null;
-            this.paginator.refresh();
+            this.valuesPaginator.urlParams = query ? {query: query} : null;
+            this.valuesPaginator.refresh();
         },
 
         clearValues: function() {