189b52d85ed8ca2ded8b65113ca62a03194aac60
Ryan O'Hara <oharar@email.chop.edu>
2014-08-01 10:25:21 -0400

    Always show columns headers
    Fixes #579
    
    Signed-off-by: Ryan O'Hara <oharar@email.chop.edu>

--- src/js/cilantro/ui/tables/table.js
+++ src/js/cilantro/ui/tables/table.js
@@ -1,12 +1,13 @@
 /* global define */
 
 define([
+    'jquery',
     'underscore',
     'marionette',
     './body',
     './header',
     './footer'
-], function(_, Marionette, body, header, footer) {
+], function($, _, Marionette, body, header, footer) {
 
     // Renders a table with the thead and tfoot elements and one or more
     // tbody elements each representing a frame of data in the collection.
@@ -47,6 +48,46 @@ define([
                     this.$el.show();
                 }
             });
+            this.listenTo(this, 'render', this.resize, this);
+            _.bindAll(this, 'resize');
+            $(window).resize(this.resize);
+        },
+
+        resize: function() {
+            var tbody = $('tbody');
+
+            if (tbody.height() > 0) {
+                var rows, rlength, row, colls, clength, width, coll;
+
+                this.$el.children().each(function() {
+                    rows = this.rows;
+                    rlength = rows.length;
+                    for(var r = 0; r < rlength; r++) {
+                        row = rows[r];
+                        colls = row.cells;
+                        clength = colls.length;
+                        width = $(document).width() / clength;
+
+                        for(var c = 0; c < clength; c++) {
+                            coll = colls[c];
+                            $(coll).css('width', width);
+                            $(coll).css('max-width', width);
+                        }
+                    }
+                });
+                
+                var offset = tbody.height() + parseInt(tbody.css('top').replace('px',''));
+
+                // When width exceeds 979, bootstrap-responsive.css sets navbar-fixed-top
+                // position to static, so the same needs to be done for tbody and footer. 
+                if ($(document).width() > 979) {
+                    $('#footer').offset({top:offset});
+                    tbody.css('position','absolute');
+                } else {
+                    $('#footer').css('position','static');
+                    tbody.css('position','static');
+                }
+            }
         },
 
         showCurrentPage: function(model, num) {
